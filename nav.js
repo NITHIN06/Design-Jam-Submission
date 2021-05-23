b4d82195fe989913e5b502b04280c12b7d91355f
A. Nithin <63183221+NITHIN06@users.noreply.github.com>
2021-05-23 12:30:13 +0530

    Update nav.js

--- nav.js
+++ nav.js
@@ -8,3 +8,18 @@ navbar_links.forEach(element=>{
     })
     //this.classList.add("active")
 })
+window.addEventListener('scroll', reveal);
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+    for(var i=0; i < reveals.length; i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+        if(revealtop < windowheight-revealpoint){
+            reveals[i].classList.add('act');
+        }
+        else{
+            reveals[i].classList.remove('act');
+        }
+    }
+}
