a13923c8d3686d3e1b5521cc579fb879c522371f
NITHIN06 <63183221+NITHIN06@users.noreply.github.com>
2021-05-24 11:33:21 +0530

    updates

--- nav.js
+++ nav.js
@@ -11,15 +11,27 @@ navbar_links.forEach(element=>{
 window.addEventListener('scroll', reveal);
 function reveal(){
     var reveals = document.querySelectorAll('.reveal');
-    for(var i=0; i < reveals.length; i++){
+    for(let val of reveals){
         var windowheight = window.innerHeight;
-        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealtop = val.getBoundingClientRect().top;
         var revealpoint = 150;
         if(revealtop < windowheight-revealpoint){
-            reveals[i].classList.add('act');
+            val.classList.add('act');
         }
         else{
-            reveals[i].classList.remove('act');
+            val.classList.remove('act');
         }
+    }
+    var topHeading = document.querySelector('.h1');
+    topHeading.classList.toggle("topHead", window.scrollY > 150);
+    var topHeading2 = document.querySelector('.middle');
+    var windowheight2 = window.innerHeight;
+    var revealtop2 = topHeading2.getBoundingClientRect().top;
+    var revealpoint2 = 150;
+    if(revealtop2 < windowheight2-revealpoint2){
+        topHeading2.classList.add('gottam');
+    }
+    else{
+        topHeading2.classList.remove('gottam');
     }
 }
