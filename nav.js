20e4485f8edfe4679aa800336c120e0526ef8aa9
A. Nithin <63183221+NITHIN06@users.noreply.github.com>
2021-05-25 13:11:38 +0530

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,13 +1,18 @@
 const navbar_links = document.querySelector(".navbar_links").querySelectorAll("a");
-
 console.log(navbar_links)
 navbar_links.forEach(element=>{
     element.addEventListener("click",function(){
         navbar_links.forEach(link=>link.classList.remove("active"))
         this.classList.add("active")
+        document.getElementById("check").checked = false;
     })
     //this.classList.add("active")
 })
+var widthOfScreen = window.innerWidth;
+var con = document.querySelector('.cont');
+var con1 = document.querySelector('.cont1');
+con.classList.toggle("reveal", widthOfScreen > 858);
+con1.classList.toggle("reveal", widthOfScreen > 858);
 window.addEventListener('scroll', reveal);
 function reveal(){
     var reveals = document.querySelectorAll('.reveal');
@@ -33,5 +38,31 @@ function reveal(){
     }
     else{
         topHeading2.classList.remove('gottam');
+    }
+    var on1 = document.querySelector('.js1');
+    var on2 = document.querySelector('.tx');
+    var on3 = document.querySelector('.box');
+    var on4 = on1.getBoundingClientRect().top;
+    
+    if(on4 < windowheight2){
+        on3.classList.add('gottam1');
+        on2.classList.add('gottam2');
+    }
+    else{
+        on3.classList.remove('gottam1');
+        on2.classList.remove('gottam2');
+    }
+    var on11 = document.querySelector('.js2');
+    var on12 = document.querySelector('.tx1');
+    var on13 = document.querySelector('.box1');
+    var on14 = on11.getBoundingClientRect().top;
+    
+    if(on14 < windowheight2){
+        on13.classList.add('pipe');
+        on12.classList.add('pipe2');
+    }
+    else{
+        on13.classList.remove('pipe');
+        on12.classList.remove('pipe2');
     }
 }
